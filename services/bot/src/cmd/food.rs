cdab617f19e4c36568dd050243257aafff8ff72a
devldavydov <devldavydov@yandex.ru>
2025-01-01 17:02:20 +0300

    Add other food commands

--- services/bot/src/cmd/food.rs
+++ services/bot/src/cmd/food.rs
@@ -8,10 +8,13 @@ use html::{
 use model::Food;
 use std::sync::Arc;
 use storage::{Storage, StorageError};
-use teloxide::{prelude::*, types::InputFile};
+use teloxide::{
+    prelude::*,
+    types::{InputFile, ParseMode},
+};
 
 use crate::{
-    messages::{ERR_EMPTY, ERR_INTERNAL, ERR_WRONG_COMMAND, OK},
+    messages::{ERR_EMPTY, ERR_FOOD_NOT_FOUND, ERR_INTERNAL, ERR_WRONG_COMMAND, OK},
     HandlerResult,
 };
 
@@ -31,8 +34,17 @@ pub async fn process_food_command(
         "set" => {
             food_set(bot, chat_id, args[1..].to_vec(), stg).await?;
         }
+        "st" => {
+            food_set_template(bot, chat_id, args[1..].to_vec(), stg).await?;
+        }
         "list" => {
             food_list(bot, chat_id, stg).await?;
+        }
+        "find" => {
+            food_find(bot, chat_id, args[1..].to_vec(), stg).await?;
+        }
+        "del" => {
+            food_del(bot, chat_id, args[1..].to_vec(), stg).await?;
         }
         _ => {
             log::error!("unknown command");
@@ -117,6 +129,51 @@ async fn food_set(
     Ok(())
 }
 
+async fn food_set_template(
+    bot: Bot,
+    chat_id: ChatId,
+    args: Vec<&str>,
+    stg: Arc<Box<dyn Storage>>,
+) -> HandlerResult {
+    if args.len() != 1 {
+        log::error!("wrong args count");
+        bot.send_message(chat_id, ERR_WRONG_COMMAND).await?;
+        return Ok(());
+    }
+
+    // Call storage
+    let food = match stg.get_food(args.first().unwrap()) {
+        Err(err) => {
+            log::error!("get food error: {err}");
+            if stg.is_storage_error(StorageError::NotFound, &err) {
+                bot.send_message(chat_id, ERR_FOOD_NOT_FOUND).await?;
+            } else {
+                bot.send_message(chat_id, ERR_INTERNAL).await?;
+            }
+            return Ok(());
+        }
+        Ok(f) => f,
+    };
+
+    bot.send_message(
+        chat_id,
+        format!(
+            "f,st,{},{},{},{:.2},{:.2},{:.2},{:.2},{}",
+            food.key,
+            food.name,
+            food.brand,
+            food.cal100,
+            food.prot100,
+            food.fat100,
+            food.carb100,
+            food.comment
+        ),
+    )
+    .await?;
+
+    Ok(())
+}
+
 async fn food_list(bot: Bot, chat_id: ChatId, stg: Arc<Box<dyn Storage>>) -> HandlerResult {
     // Call storage
     let f_lst = match stg.get_food_list() {
@@ -172,6 +229,80 @@ async fn food_list(bot: Bot, chat_id: ChatId, stg: Arc<Box<dyn Storage>>) -> Han
         InputFile::memory(doc.build()).file_name("food.html"),
     )
     .await?;
+
+    Ok(())
+}
+
+async fn food_find(
+    bot: Bot,
+    chat_id: ChatId,
+    args: Vec<&str>,
+    stg: Arc<Box<dyn Storage>>,
+) -> HandlerResult {
+    if args.len() != 1 {
+        log::error!("wrong args count");
+        bot.send_message(chat_id, ERR_WRONG_COMMAND).await?;
+        return Ok(());
+    }
+
+    // Call storage
+    let food = match stg.find_food(args.first().unwrap()) {
+        Err(err) => {
+            log::error!("find food error: {err}");
+            if stg.is_storage_error(StorageError::EmptyList, &err) {
+                bot.send_message(chat_id, ERR_EMPTY).await?;
+            } else {
+                bot.send_message(chat_id, ERR_INTERNAL).await?;
+            }
+            return Ok(());
+        }
+        Ok(f) => f,
+    };
+
+    let mut res = format!("");
+    for (i, f) in food.iter().enumerate() {
+        res.push_str(&format!("<b>Ключ: </b>{}\n", f.key));
+        res.push_str(&format!("<b>Наименование:</b> {}\n", f.name));
+        res.push_str(&format!("<b>Бренд:</b> {}\n", f.brand));
+        res.push_str(&format!("<b>ККал100:</b> {:.2}\n", f.cal100));
+        res.push_str(&format!("<b>Бел100:</b> {:.2}\n", f.prot100));
+        res.push_str(&format!("<b>Жир100:</b> {:.2}\n", f.fat100));
+        res.push_str(&format!("<b>Угл100:</b> {:.2}\n", f.carb100));
+        res.push_str(&format!("<b>Комментарий:</b> {}\n", f.comment));
+
+        if i != res.len() - 1 {
+            res.push_str("\n");
+        }
+    }
+
+    bot.send_message(chat_id, res)
+        .parse_mode(ParseMode::Html)
+        .await?;
+
+    Ok(())
+}
+
+async fn food_del(
+    bot: Bot,
+    chat_id: ChatId,
+    args: Vec<&str>,
+    stg: Arc<Box<dyn Storage>>,
+) -> HandlerResult {
+    if args.len() != 1 {
+        log::error!("wrong args count");
+        bot.send_message(chat_id, ERR_WRONG_COMMAND).await?;
+        return Ok(());
+    }
+
+    // Call storage
+    if let Err(err) = stg.delete_food(args.first().unwrap()) {
+        log::error!("del food error: {err}");
+        // TODO: if journal violation
+        bot.send_message(chat_id, ERR_INTERNAL).await?;
+        return Ok(());
+    };
+
+    bot.send_message(chat_id, OK).await?;
 
     Ok(())
 }
