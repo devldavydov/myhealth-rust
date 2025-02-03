2e95e3d1af9a6c6ca0e6019deb435f3552a62649
devldavydov <devldavydov@yandex.ru>
2025-02-03 17:12:26 +0000

    Add backup command

--- services/bot/src/cmd/maintenance.rs
+++ services/bot/src/cmd/maintenance.rs
@@ -1,10 +1,23 @@
-use std::sync::Arc;
+use std::{io::Read, sync::Arc};
 
+use anyhow::Context;
 use chrono_tz::Tz;
+use flate2::{bufread::GzEncoder, Compression};
+use serde_json::json;
 use storage::Storage;
-use teloxide::{prelude::*, types::ChatId, Bot};
+use teloxide::{
+    prelude::*,
+    types::{ChatId, InputFile},
+    Bot,
+};
+use types::timestamp::Timestamp;
 
-use crate::{messages::ERR_WRONG_COMMAND, HandlerResult};
+use crate::{
+    messages::{ERR_INTERNAL, ERR_WRONG_COMMAND},
+    HandlerResult,
+};
+
+use super::format_timestamp;
 
 pub async fn process_maintenance(
     bot: Bot,
@@ -21,7 +34,7 @@ pub async fn process_maintenance(
 
     match *args.first().unwrap() {
         "backup" => {
-            backup(bot, chat_id, args[1..].to_vec(), stg, tz).await?;
+            backup(bot, chat_id, stg, tz).await?;
         }
         _ => {
             log::error!("unknown command");
@@ -32,14 +45,28 @@ pub async fn process_maintenance(
     Ok(())
 }
 
-async fn backup(
-    bot: Bot,
-    chat_id: ChatId,
-    args: Vec<&str>,
-    stg: Arc<Box<dyn Storage>>,
-    tz: Tz,
-) -> HandlerResult {
+async fn backup(bot: Bot, chat_id: ChatId, stg: Arc<Box<dyn Storage>>, tz: Tz) -> HandlerResult {
     // Get storage data for backup
+    let res = stg.backup();
+    if let Err(err) = res {
+        log::error!("backup error: {err}");
+        bot.send_message(chat_id, ERR_INTERNAL).await?;
+        return Ok(());
+    }
+
+    let data = serde_json::to_vec(&json!(res.unwrap())).context("failed to serde JSON backup")?;
+    let mut gz = GzEncoder::new(&data[..], Compression::best());
+    let mut out = Vec::new();
+    gz.read_to_end(&mut out).context("failed to gzip backup")?;
+
+    bot.send_document(
+        chat_id,
+        InputFile::memory(out).file_name(format!(
+            "backup_{}.json.gz",
+            format_timestamp(&Timestamp::now(), "%d.%m.%Y", tz)
+        )),
+    )
+    .await?;
 
     Ok(())
 }
